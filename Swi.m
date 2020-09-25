04eb5dd395c8c54c725bc16f2e932400e69e4d53
Jonathan-Al-Saadi <69520285+Jonathan-Al-Saadi@users.noreply.github.com>
2020-09-25 15:25:59 +0200

    Added the ability to calculate SWI

--- Swi.m
+++ Swi.m
@@ -1,25 +1,57 @@
+function vol = Swi();
 
-mag = niftiread('/Users/jonathan/Desktop/s_20200609_Gris7hjarna/Pig7mge3d.nii/MG/image004.nii');
-phase = niftiread('/Users/jonathan/Desktop/s_20200609_Gris7hjarna/Pig7mge3d.nii/PH/image004.nii');
-mag = rot90_3D(mag, 1, 1);
-phase = rot90_3D(phase, 1, 1);
+%% Reading the phase and magnitude, then determining dimensions
+mag = niftiread('/Users/jonathan/Desktop/s_20200609_Gris7hjarna/Pig7mge3d.nii/MG/image004.nii'); [magSizeX, magSizeY, magSizeZ] = size(mag);
+phase = niftiread('/Users/jonathan/Desktop/s_20200609_Gris7hjarna/Pig7mge3d.nii/PH/image004.nii'); [phaseSizeX, phaseSizeY, phaseSizeZ] = size(phase);
 
+%% Rotating the magnitude and phase images
+mag = rot90_3D(mag, 1, 1); phase = rot90_3D(phase, 1, 1);
+
+%%Calculating the comp according to the euler equation m*e^(i*phase)
 comp = mag.*exp(i*(phase + pi));
 
+%%Creating a hanning filter, according to Hecke et al.
+filterSize = 70;
+filter = hanning(filterSize)*hanning(filterSize)';
 
-s = 512;
-z = 512;
+%Setting the filter size to equal the array size
+pad = (magSizeX-filterSize)/2; filter = padarray(filter, [pad pad]);
 
-filter = hanning(70)*hanning(70)';
+%Shifting the filter
+filter = fftshift(filter);
 
-pad = (s-70)/2;
-
-filter = padarray(filter, [pad pad]);
-
-for o = 1:z
-    lpslice = ifft2(fft2(comp(:,:,o)).*fftshift(filter));
+%For each slice, calculating the frequency comp then applying the filter to
+%that and inverse forierransfroming which creates a lowpass filter stored
+%in lpslice. To create a high pass filter, every comp slice is divided by the
+%corrosponding lpslice and the value is assigned to hpcomp.
+for o = 1:magSizeZ
+    lpslice = ifft2(fft2(comp(:,:,o)).*filter);
     hpcomp(:,:,o) = comp(:,:,o)./lpslice;
 end
 
+%% Applying the inverse of the euler equation we get phase. 
 hpphase = angle(hpcomp);
 
+phaseMask = zeros(magSizeX, magSizeY, magSizeZ);
+
+%% Rescaling all values between -pi and 0 to 0 to 1. Everything above 0 is set to 1.
+for slice = 1:magSizeZ
+    for yDim = 1:magSizeY
+        for xDim = 1:magSizeX
+            if hpphase(xDim, yDim, slice) >= 0
+                phaseMask(xDim, yDim, slice) = 1;
+            else
+                phaseMask(xDim, yDim, slice) = hpphase(xDim, yDim, slice)/pi +1;
+            end
+        end
+    end
+end
+
+%% Creating the SWI
+%phaseMask ^4 then multiplying it by the magnitude image. 4 comes from
+%Hacke et al's original paper.
+vol = phaseMask.^4.*mag;
+
+
+    
+    
