8fe8cf521f3fa85d4c5521bf09a651c96ffc1f5c
Jonathan-Al-Saadi <69520285+Jonathan-Al-Saadi@users.noreply.github.com>
2020-09-29 21:16:23 +0200

    ghgh

--- magExport_scramble.m
+++ magExport_scramble.m
@@ -1,7 +1,7 @@
-function magExport_scramble(filename, samplenumber)
+function magExport_scramble(filename, samplenumber, format)
 
 %Mapping the image from 0 - 256
-mapped_image = mapImage(filename);
+mapped_image = mapImage(filename, format);
 
 %Creating a folder with encrypted name, leaving a key in the folder.
 dirPath = scrambler(samplenumber);
@@ -9,8 +9,18 @@ dirPath = scrambler(samplenumber);
 % Adding the images to that folder
 cd(dirPath);
 
-for slice = 1:size(mapped_image, 3)
-imwrite(mapped_image(:,:,slice), strcat(sprintf('image%.3d', slice), '.tif'));
+
+switch format
+    case '.tif'
+        imwrite(mapped_image(:,:,slice), strcat(sprintf('image%.3d', slice), '.tif'));
+    case '.nii'
+        niftiwrite(mapped_image, dirPath);
+    case '.dcm'
+        [x, y, slice] = size(mapped_image);
+        mapped_image = uint16(mapped_image);
+        dicomwrite(reshape(mapped_image, [x,y,1,slice]), strcat(dirPath, '.dcm'));
+    otherwise
+        warning('Unexpected format. No export created. Use ''.tif'', ''.dicom'' or ''.nii''')
 end
 
 cd('..');
--- mapImage.m
+++ mapImage.m
@@ -1,4 +1,4 @@
-function mapped_image = mapImage(filename)
+function mapped_image = mapImage(filename, format)
 % Magexport takes a file and then exports it as a series of images in a
 % directory to be used in imageJ. The directory name is then encrypted to
 % blind the user.
@@ -10,17 +10,20 @@ function mapped_image = mapImage(filename)
 % this seems to work).
 mag = niftiread(filename); mag = rot90_3D(mag, 1, 3); mag = imrotate(mag, 90); mag = imrotate3(mag, 90, [0 1 0]); mag = imrotate(mag, -90);
 
-
-%Rescaling the color map
-minD = min(0);
-maxD = max(300000);
-mapped_image = (double(mag) - minD) ./ (maxD - minD);
-ncmap = size(colormap, 1);
-mapped_image = mapped_image .* ncmap;
-if ncmap == 2
-   mapped_image = mapped_image >= 0.5;  %logical
-elseif ncmap <= 256
-   mapped_image = uint8(mapped_image);
+%Rescaling the color map if it is an image.
+if(strcmp(format, '.img')) %|| strcmp(format, '.dcm'))
+    minD = min(0);
+    maxD = max(300000);
+    mapped_image = (double(mag) - minD) ./ (maxD - minD);
+    ncmap = size(colormap, 1);
+    mapped_image = mapped_image .* ncmap;
+    if ncmap == 2
+        mapped_image = mapped_image >= 0.5;  %logical
+    elseif ncmap <= 256
+        mapped_image = uint8(mapped_image);
+    else
+        mapped_image = uint16(mapped_image);
+    end
 else
-   mapped_image = uint16(mapped_image);
+    mapped_image = mag;
 end
